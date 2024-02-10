07b4c49b114c019910908c9a62408857731bf549
imanninen <igor.manninen@gmail.com>
2024-02-10 11:30:12 +0200

    Initial commit

---
+++ aboba.gd
@@ -0,0 +1,14 @@
+extends RigidBody2D
+
+var gg: int = 0
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print("hell")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	gg += 1
+	if gg % 60 == 0:
+		print(gg)
+	
