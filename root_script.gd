b600db19bda1ff54ae44dbae91fd793d99f58e4c
imanninen <igor.manninen@gmail.com>
2024-02-11 18:06:20 +0200

    draw all cells and mark them visited they are or not

--- root_script.gd
+++ root_script.gd
@@ -26,20 +26,29 @@ func _process(delta):
 		cell_x = pos_x_div
 		cell_y = pos_y_div
 		current_cell = new_current_cell
+		field[pos_x_div][pos_y_div].is_touched = true
 		queue_redraw()
 	
 func _draw():
-	draw_rect(Rect2(cell_x*64, cell_y*64, 64, 64), Color.RED, false)	
-	print(cell_x, cell_y)
 	
+	print(cell_x, cell_y)
+	for i in range(10):
+		for j in range(10):
+			if field[i][j].is_touched:
+				draw_rect(Rect2(i*64 + 1, j*64 + 1, 63, 63), Color.GREEN, false)
+			else:
+				draw_rect(Rect2(i*64 + 1, j*64 + 1, 63, 63), Color.RED, false)
+				
 func init_field():
 	for i in range(10):
 		var tmpArr = []
 		for j in range(10):
 			var tmpCell = Cell.new(false, str(i) + "-" + str(j))
 			tmpArr.append(tmpCell)
+		
 		field.append(tmpArr)
-			
+	cell_x = 0
+	cell_y = 0
 	
 class Cell:
 	var is_touched: bool
